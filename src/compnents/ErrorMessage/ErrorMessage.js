4e4e76d33616640bad28e4973b035e9f98a6f5db
Min <barrs.mindy@gmail.com>
2019-03-21 14:47:03 -0400

    Fixed some minor issues with the styling.

--- src/compnents/ErrorMessage/ErrorMessage.js
+++ src/compnents/ErrorMessage/ErrorMessage.js
@@ -6,8 +6,10 @@ import "./ErrorMessage.css";
 
 const ErrorMessage = (props) => {
     return(
-        <div className="ui floating message">
-            <p>{props.errorMessage}</p>
+        <div className="display-area">
+            <div className="ui floating message">
+                <p>{props.errorMessage}</p>
+            </div>
         </div>
     );
 } 
